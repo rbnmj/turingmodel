96ac8921058fd500bbb1923ac814e5e7edb744b6
jardner1 <jardner1@uni-potsdam.de>
2023-10-04 19:36:36 +0200

    added crowding + growthrate cleanup

--- modellib/invasion_growthrateV3_logV2.py
+++ modellib/invasion_growthrateV3_logV2.py
@@ -40,12 +40,12 @@ number_steps = 20000
 t = np.linspace(0, t_end, number_steps)
 
 # dispersal adaptability
-k_1_range = np.geomspace(0.1, 10, 50)
-k_2_range = np.geomspace(0.1, 10, 50)
+k_1_range = np.geomspace(0.1, 10, 100)
+k_2_range = np.geomspace(0.1, 10, 100)
 
 # maximum dispersal rate
-d_Hmax1 = 10**-2
-d_Hmax2 = 10**-2
+d_Hmax1 = 10**-3
+d_Hmax2 = 10**-3
 
 # var = [N_a, N_b, A_a, A_b, H_1a, H_1b, H_2a, H_2b]
 var0 = [2, 2.5, 2.5, 2, 0.08, 0.4, 10**-6, 0, 0, 0, 0, 0, 0, 0]
@@ -83,17 +83,17 @@ for k_2 in tqdm(k_2_range):
     i += 1
 
 # saving results
-np.savetxt("./data/growthrateV3_10-3/H1Slope10-3_logV2.csv", slopeH1, delimiter=",")
-np.savetxt("./data/growthrateV3_10-3/H2Slope10-3_logV2.csv", slopeH2, delimiter=",")
+np.savetxt("./data/growthrate/103/H1Slope10-3.csv", slopeH1, delimiter=",")
+np.savetxt("./data/growthrate/103/H2Slope10-3.csv", slopeH2, delimiter=",")
 
 # # loading results
-# slopeH1 = np.loadtxt("./data/growthrateV3_10-2/H1Slope10-2_log2.csv", delimiter=",")
-# slopeH2 = np.loadtxt("./data/growthrateV3_10-2/H2Slope10-2_log2.csv", delimiter=",")
+# slopeH1 = np.loadtxt("./data/growthrateV3_10-3/H1Slope10-3_logV2.csv", delimiter=",")
+# slopeH2 = np.loadtxt("./data/growthrateV3_10-3/H2Slope10-3_logV2.csv", delimiter=",")
 
 d_Hmax = "10-3"
 
 # plotting
-k_1_range = np.geomspace(0.1, 10, 50)
+k_1_range = np.geomspace(0.1, 10, 100)
 ticks = np.append(k_1_range, 10)
 
 # total change
@@ -111,11 +111,11 @@ plt.xlabel('$k_1$')
 plt.ylabel('$k_2$')
 plt.title(
     'Change of heterotroph densitiy \n $d_{H_{max}} = $' + str(d_Hmax) + ', H2 invader')
-plt.savefig('./output/invasion_growthrateV3_'+str(d_Hmax)+'_H1-H2_logV2.png')
+plt.savefig('./output/final/invasion_growthrateV3_'+str(d_Hmax)+'_H1-H2_logV2.png')
 
 # H1 change
 fig, ax2 = plt.subplots()
-k_1_range = np.geomspace(0.1, 10, 50)
+k_1_range = np.geomspace(0.1, 10, 100)
 ticks = np.append(k_1_range, 10)
 ax2 = sns.heatmap((slopeH1), cmap="BrBG", square=True, cbar=False)#, vmin=-10**-7, vmax=10**-7)
 cbar2 = ax2.figure.colorbar(ax2.collections[0])
@@ -129,13 +129,13 @@ plt.grid(color='black', linewidth=0.5, linestyle='--')
 plt.xlabel('$k_1$')
 plt.ylabel('$k_2$')
 plt.title('Growth rate of $H_1$ \n $d_{H_{max}} = $' + str(d_Hmax) + ', H2 invader')
-plt.savefig('./output/invasion_growthrateV3_'+str(d_Hmax)+'_H1_logV2.png')
+plt.savefig('./output/final/invasion_growthrateV3_'+str(d_Hmax)+'_H1_logV2.png')
 
 # H2 change
 fig, ax3 = plt.subplots()
-k_1_range = np.geomspace(0.1, 10, 50)
+k_1_range = np.geomspace(0.1, 10, 100)
 ticks = np.append(k_1_range, 10)
-ax3 = sns.heatmap((slopeH2), cmap="BrBG", square=True, cbar=False, vmin=-0.2, vmax=0.2)
+ax3 = sns.heatmap((slopeH2), cmap="BrBG", square=True, cbar=False, vmin=-0.04, vmax=0.04)
 cbar3 = ax3.figure.colorbar(ax3.collections[0])
 cbar3.set_label('$H_2$', rotation=270, labelpad=12)
 ax3.set_xticks(np.linspace(0, len(k_1_range), 3))
@@ -143,8 +143,9 @@ ax3.set_yticks(np.linspace(0, len(k_1_range), 3))
 ax3.invert_yaxis()
 ax3.set_xticklabels(("$10^{-1}$", "$10^{0}$", "$10^{1}$"), rotation=0)
 ax3.set_yticklabels(("$10^{-1}$", "$10^{0}$", "$10^{1}$"))
-plt.grid(color='black', linewidth=0.5, linestyle='--')
+#plt.grid(color='black', linewidth=0.5, linestyle='--')
+ax3.plot([0, len(k_1_range)], [0, len(k_1_range)], color="black", linestyle="--", linewidth=0.75)
 plt.xlabel('$k_1$')
 plt.ylabel('$k_2$')
 plt.title('Growth rate of $H_2$ \n $d_{H_{max}} = $' + str(d_Hmax) + ', H2 invader')
-plt.savefig('./output/invasion_growthrateV3_'+str(d_Hmax)+'_H2_logV2.png')
+plt.savefig('./output/final/invasion_growthrateV3_'+str(d_Hmax)+'_H2_logV2.png')
